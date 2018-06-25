540cca5620e690a318fddff77e4f2044cba4d569
Gareth Wright <GarethWright@users.noreply.github.com>
2018-06-25 14:52:56 +0100

    Update and rename barbiegirl.ps1 to Rick.ps1

--- examples/Rick.ps1
+++ examples/Rick.ps1
@@ -1,11 +1,11 @@
 # From https://github.com/EmpireProject/Empire/blob/master/lib/modules/powershell/trollsploit/thunderstruck.py
-Function Invoke-BarbieGirl
+Function Invoke-RickRoll
 {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $False, Position = 0)]
         [ValidateNotNullOrEmpty()]
-        [String] $VideoURL = "https://youtu.be/ZyhrYis509A?t=13"
+        [String] $VideoURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     )
     
     Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
@@ -15,11 +15,11 @@ Function Invoke-BarbieGirl
     $IEComObject.visible = $False
     $IEComObject.navigate($VideoURL)
     Start-Sleep -s 5
-    $EndTime = (Get-Date).addseconds(90)
+    $EndTime = (Get-Date).addseconds(199)
     # ghetto way to do this but it basically presses volume up to raise volume in a loop for 90 seconds
     do {
        $WscriptObject = New-Object -com wscript.shell
        $WscriptObject.SendKeys([char]175)
     }
     until ((Get-Date) -gt $EndTime)
-} Invoke-BarbieGirl
+} Invoke-RickRoll
